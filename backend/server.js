bb85e0a5a11ed85559eb60095f7b9450991e2dfd
AVGfx <avgfx15@gmail.com>
2026-02-16 15:29:24 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5003;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`AVGfx app listening on port ${port}!`));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <h1>AVGfx</h1>
+        <p>Audio Video Graphics Special Effects</p>
+      </div>
+      <div>
+        <h2>Section 1</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+          repellendus aliquid qui commodi ipsam molestiae. Laudantium provident
+          impedit quibusdam reprehenderit illo explicabo quasi! Aperiam animi
+          sed optio tenetur facilis voluptas sapiente mollitia possimus omnis,
+          iure rem nihil! Ea quas nihil, unde odit consequatur est, voluptate,
+          commodi autem error id aperiam.
+        </p>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default App;
